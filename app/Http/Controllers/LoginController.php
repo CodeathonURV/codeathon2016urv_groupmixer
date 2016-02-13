d1cbbfbc0a81a1ffb3eff96354b931bcb73267be
Idir Ouhab Meskine <ouhab.idir@gmail.com.com>
2016-02-13 13:50:02 +0100

    Add login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commands\LoginCommand;
+use App\Commands\RoleCommand;
+use Illuminate\Http\Request;
+use Redirect;
+use Validator;
+use View;
+
+class LoginController extends Controller
+{
+    /**
+     * @var LoginCommand
+     */
+    private $loginCommand;
+    /**
+     * @var RoleCommand
+     */
+    private $roleCommand;
+
+    /**
+     * LoginController constructor.
+     * @param LoginCommand $loginCommand
+     * @param RoleCommand $roleCommand
+     */
+    public function __construct(LoginCommand $loginCommand, RoleCommand $roleCommand)
+    {
+        $this->loginCommand = $loginCommand;
+        $this->roleCommand = $roleCommand;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function createLogin()
+    {
+        return View::make('login.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
+    public function saveLogin(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+            [
+                'dni' => 'required',
+                'password' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator)
+                ->withInput();
+        }
+
+
+        if ($this->loginCommand->checkLogin($request->all())) {
+            return Redirect::route('dashboard');
+        } else {
+            return Redirect::back();
+        }
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function createRegister()
+    {
+        $roles = $this->roleCommand->listRoles(['id', 'name']);
+        return View::make('login.register', compact('roles'));
+
+    }
+
+    /**
+     * @param Request $request
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
+    public function saveRegister(Request $request)
+    {
+
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'surnames' => 'required',
+                'dni' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:3|confirmed',
+                'password_confirmation' => 'required|min:3',
+                'user_type' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+
+            return Redirect::back()->withErrors($validator)
+                ->withInput();
+        }
+
+        $this->loginCommand->save($request->all());
+
+        return Redirect::route('login.create');
+    }
+
+}
